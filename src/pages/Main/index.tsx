f5823fdb788812a5bc4e49d7420ce04df99d4afa
David <davidsimonkastle@gmail.com>
2019-09-21 23:26:14 -0300

    basic routing

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+function Main() {
   return (
     <div>
       <h1>OSRS Helper</h1>
@@ -8,4 +8,4 @@ function App() {
   );
 }
 
-export default App;
+export default Main;
